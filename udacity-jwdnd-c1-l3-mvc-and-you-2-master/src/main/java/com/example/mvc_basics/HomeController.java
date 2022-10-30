de774c85dd11c42527a566b630ead3b88606effe
leroneb <lerone.bleasdille@gmail.com>
2022-10-29 22:05:36 -0400

    Adding greeting page with name recognition.

--- udacity-jwdnd-c1-l3-mvc-and-you-2-master/src/main/java/com/example/mvc_basics/HomeController.java
+++ udacity-jwdnd-c1-l3-mvc-and-you-2-master/src/main/java/com/example/mvc_basics/HomeController.java
@@ -3,6 +3,8 @@ package com.example.mvc_basics;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.GetMapping;
 
 import java.time.Instant;
 
@@ -11,10 +13,17 @@ public class HomeController {
 
     @RequestMapping("/home")
     public String getHomePage(Model model) {
-        model.addAttribute("welcomeMessage", Instant.now().toString());
+        model.addAttribute("welcomeMessage", "Hi, Hello @ " + Instant.now().toString());
         return "home";
     }
-
+    
+    
+    @GetMapping("/greeting")
+    public String greeting(@RequestParam(name="name", required=false, defaultValue="World") String name, Model model) {
+        model.addAttribute("name", name);
+        // name of template to return
+        return "greeting";
+    }
 }
 
 
